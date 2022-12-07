d06a3d9a4e20fab32842fbae34b4a5fd1179829f
Margo <91619502+MargoRT8921@users.noreply.github.com>
2022-12-07 15:00:39 -0500

    Add files via upload

---
+++ lab7/lab7/Animal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lab7
+{
+    public abstract class Animal
+    {
+        public string Country { get; set; } 
+        public string HideFromOtherAnimals { get; set; }
+        public string Name { get; set; }
+        public string WhatAnimal { get; set; }
+
+        public Animal(string country, string hide, string name, string what)
+        {
+            Country = country;
+            HideFromOtherAnimals = hide;
+            Name = name;
+            WhatAnimal = what;
+        }
+        public void Deconstruct()
+        {
+            Console.WriteLine("Deconstruct()");
+        }
+        public string GetClassificationAnimal()
+        {
+            return Name;
+        }
+
+    }
+}
